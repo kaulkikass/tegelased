3c08d336fc82ca261beaa9b115baeaaaeed9e1c8
kaulkikass <kaulkikass@gmail.com>
2022-07-18 21:41:47 +0300

    React homework 13 - add functionality to add characters to  an array. display them and remove, display how many characters are chosen, delete all chosen characters

--- src/Pages/Tegelased.js
+++ src/Pages/Tegelased.js
@@ -1,29 +1,69 @@
 import { useState } from "react";
 
 function Tegelased() {
+    const [valitudTegelased, muudaValitudTegelased] = useState([]);
     const [tegelane, n2itaTegelast] = useState('');
     const tegelased = [
-        {eesnimi: 'Mickey', perenimi: 'Mouse', elukoht: 'Disneyland'},
-        {eesnimi: 'Minnie', perenimi: 'Mouse', elukoht: 'Disneyland'},
-        {eesnimi: 'Winnie', perenimi: 'Pooh', elukoht: 'Hundred Acre Wood'},
-        {eesnimi: 'Roo', perenimi: 'Kangaroo', elukoht: 'Hundred Acre Wood'},
-        {eesnimi: 'Scooby', perenimi: 'Doo', elukoht: 'Crystal Cove'}
+        {eesnimi: 'Mickey', perenimi: 'Mouse', elukoht: 'Disneyland', vanus: '22'},
+        {eesnimi: 'Minnie', perenimi: 'Mouse', elukoht: 'Disneyland', vanus: '33'},
+        {eesnimi: 'Winnie', perenimi: 'Pooh', elukoht: 'Hundred Acre Wood', vanus: '44'},
+        {eesnimi: 'Roo', perenimi: 'Kangaroo', elukoht: 'Hundred Acre Wood', vanus: '55'},
+        {eesnimi: 'Scooby', perenimi: 'Doo', elukoht: 'Crystal Cove', vanus:'66'}
     ];
     const kuvaNimi = (tegelane) => {
         console.log(tegelane.eesnimi);
     }
+
+    const valiTegelane = (tegelane) => {
+        valitudTegelased.push(tegelane);
+        console.log(valitudTegelased);
+    }
+    const eemaldaValitudTegelastest = (index) => {
+        valitudTegelased.splice(index,1);
+        muudaValitudTegelased(valitudTegelased.slice());
+    }
+    const lisaValitudTegelastesse = (tegelane) => {
+        valitudTegelased.push(tegelane);
+        muudaValitudTegelased(valitudTegelased.slice());
+    }
+    const tyhjenda = () => {
+        muudaValitudTegelased([]);
+    }
     return ( 
+
     <div>
-        {tegelased.map(tegelane =>
-        <div onClick={() => n2itaTegelast(tegelane.eesnimi)}>
+        {tegelased.map((tegelane, index) =>
+        <div key={index} onClick={() => n2itaTegelast(tegelane.eesnimi)}>
                 <div>{tegelane.eesnimi}</div>
                 <div>{tegelane.perenimi}</div>
                 <div>{tegelane.elukoht}</div>
+                <div>{tegelane.vanus}</div>
                 <button onClick={() => kuvaNimi(tegelane)}>Saada eesnimi</button>
+                <button onClick={() => valiTegelane(tegelane)}>Vali tegelane</button>
                 <br />
         </div>
         )}
+        <br />
+        <button onClick={() => tyhjenda()}>Tühjenda</button>
+        {valitudTegelased.length === 0 && <div>Valitud pole ühtegi tegelast</div>}
+        {valitudTegelased.length > 0 &&
+        <div>
+            
+            Valitud tegelased:
+            {valitudTegelased.map((tegelane, index) =>
+            <div key={index}>
+                <div>{tegelane.eesnimi}</div>
+                <div>{tegelane.perenimi}</div>
+                <div>{tegelane.elukoht}</div>
+                <div>{tegelane.vanus}</div>
+                <button onClick={() => eemaldaValitudTegelastest(index)}>x</button>
+                <button onClick={() => lisaValitudTegelastesse(tegelane)}>+</button>
+            </div>
+            )}
+            <div>Valitud on {valitudTegelased.length} tegelast</div>
+        </div>}
         { tegelane !== '' && <div>Klikkisid tegelase {tegelane} peal</div> }
+
     </div> 
     );
 }
